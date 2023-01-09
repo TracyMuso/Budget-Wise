2ae5e1440ffbb4fd9c1a816d69194525ba929ce5
Elfin Git <musongoletracy@gmail.com>
2023-01-09 23:04:15 +0200

    add cancan and fix linters

--- app/controllers/splash_controller.rb
+++ app/controllers/splash_controller.rb
@@ -1,10 +1,14 @@
 class SplashController < ApplicationController
-    def index
-        return unless current_user.present?
-    
-        @categories = current_user.groups.includes(:group_categories)
-        @totals = @categories.map do |catagory|
-          catagory.group_categories.reduce(0) { |sum, num| sum + num.category.amount }
-        end
-      end
+  def index
+    return unless current_user.present?
+
+    @categories = current_user.groups.includes(:group_categories)
+    @totals = @categories.map do |catagory|
+      catagory.group_categories.reduce(0) { |sum, num| sum + num.category.amount }
+    end
+  end
 end
+
+	
+
+rails generate cancan:ability
